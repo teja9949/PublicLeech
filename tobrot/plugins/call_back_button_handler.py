8167c40f800ed088099a8edcad5d70bccb32f9c9
GautamKumar <25435568+gautamajay52@users.noreply.github.com>
2020-07-21 15:04:09 +0530

    Update call_back_button_handler.py

--- tobrot/plugins/call_back_button_handler.py
+++ tobrot/plugins/call_back_button_handler.py
@@ -132,3 +132,19 @@ async def button(bot, update: CallbackQuery):
 
     elif "|" in cb_data:
         await youtube_dl_call_back(bot, update)
+
+    elif cb_data.startswith("cancel"):
+        if len(cb_data) > 1:
+            i_m_s_e_g = await update.message.reply_text("checking..?", quote=True)
+            aria_i_p = await aria_start()
+            g_id = cb_data.split()[-1]
+            LOGGER.info(g_id)
+            try:
+                downloads = aria_i_p.get_download(g_id)
+                LOGGER.info(downloads)
+                LOGGER.info(downloads.remove(force=True))
+                await i_m_s_e_g.edit_text(f"Leech Cancelled by <a href='tg://user?id={update.from_user.id}'>{update.from_user.first_name}</a>")
+            except Exception as e:
+                await i_m_s_e_g.edit_text("<i>FAILED</i>\n\n" + str(e) + "\n#error")
+            else:
+                await update.message.delete()
